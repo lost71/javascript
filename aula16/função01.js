7ce69e85e6a2244b2982c3c46908f48c383cb998
lost71 <jaguardutra2014@gmail.com>
2024-04-05 21:37:47 -0300

    Funções - JS
    Nesta função:
    
    n % 2 === 0 verifica se o número n é divisível por 2 sem deixar resto, o que significa que é par.
    Se o número for par, a função retorna a string "par".
    Se o número não for par, a função retorna a string "ímpar".
    Você pode chamar a função parinpar com diferentes números como argumento para verificar se são pares ou ímpares.

---
+++ aula16/função01.js
@@ -0,0 +1,11 @@
+function parinpar(n) {
+    if (n % 2 === 0) {
+        return "par";
+    } else {
+        return "ímpar";
+    }
+}
+
+// Exemplos de uso:
+console.log(parinpar(4)); // Saída: "par"
+console.log(parinpar(7)); // Saída: "ímpar"
