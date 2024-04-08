86c21d2a5ce1133741f858563791efc7347adfcd
lost71 <jaguardutra2014@gmail.com>
2024-04-08 16:33:45 -0300

    -inicio js

--- aula16/aula12ex/site js html css/script.js
+++ aula16/aula12ex/site js html css/script.js
@@ -3,18 +3,5 @@ function carregar() {
     var imagem = window.document.getElementById('imagem');
     var data = new Date();
     var hora = data.getHours();
-
-    if (hora >= 6 && hora < 12) {
-        msg.innerHTML = 'Bom dia!';
-        imagem.src = 'manha.png';
-        imagem.alt = 'Manhã';
-    } else if (hora >= 12 && hora < 18) {
-        msg.innerHTML = 'Boa tarde!';
-        imagem.src = 'tarde.png';
-        imagem.alt = 'Tarde';
-    } else {
-        msg.innerHTML = 'Boa noite!';
-        imagem.src = 'noite.png';
-        imagem.alt = 'Noite';
-    }
+    msg.innerHTML = 'Agora são ' + hora + ' horas';
 }
