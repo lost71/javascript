38e746cd08e0d307580ced948219acb48e361bf9
lost71 <jaguardutra2014@gmail.com>
2024-04-09 09:37:00 -0300

    js completo
    vamos de // verificar idade agora

--- aula16/aula12ex/site js html css/script.js
+++ aula16/aula12ex/site js html css/script.js
@@ -2,6 +2,32 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var imagem = window.document.getElementById('imagem');
     var data = new Date();
-    var hora = data.getHours();
-    msg.innerHTML = 'Agora são ' + hora + ' horas';
+    //var hora = data.getHours();
+    var hora = 22
+    msg.innerHTML = "Agora são " + hora + " horas.";
+
+    var body = document.body;
+
+    if (hora >= 0 && hora < 12) {
+        // Manhã
+        body.style.background = "linear-gradient(135deg, #B3E5FC, #FFC371)";
+        header.style.color = "#FFF";
+        section.style.background = "#FFCC80";
+        footer.style.color = "#7986CB";
+        imagem.src = 'manha.png';
+    } else if (hora >= 12 && hora < 18) {
+        // Tarde
+        body.style.background = "linear-gradient(135deg, #FFA07A, #FFD700)";
+        header.style.color = "#FFF";
+        section.style.background = "#FFD700";
+        footer.style.color = "#8B4513";
+        imagem.src = 'tarde.png';
+    } else {
+        // Noite
+        body.style.background = "linear-gradient(135deg, #483D8B, #000080)";
+        header.style.color = "#FFF";
+        section.style.background = "#000080";
+        footer.style.color = "#FFF";
+        imagem.src = 'noite.png';
+    }
 }
