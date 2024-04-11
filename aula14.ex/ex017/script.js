cdd3cf58aae70c0b51e2a625adb8671deba369a0
lost71 <jaguardutra2014@gmail.com>
2024-04-11 15:19:59 -0300

    tabuada
    falta mais

--- aula14.ex/ex017/script.js
+++ aula14.ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('olá')
+document.addEventListener("DOMContentLoaded", function() {
+    // Função para gerar a tabuada
+    function gerarTabuada(numero) {
+        var tabuada = "";
+        for (var i = 1; i <= 10; i++) {
+            tabuada += numero + " x " + i + " = " + (numero * i) + "<br>";
+        }
+        return tabuada;
+    }
+
+    // Elemento onde a tabuada será exibida
+    var tabuadaContainer = document.getElementById("tabuada-container");
+
+    // Gerar a tabuada do número 7 (pode ser alterado para outro número)
+    var tabuadaDoSete = gerarTabuada(7);
+
+    // Exibir a tabuada no elemento HTML
+    tabuadaContainer.innerHTML = "<h2>Tabuada do 7</h2>" + tabuadaDoSete;
+});
